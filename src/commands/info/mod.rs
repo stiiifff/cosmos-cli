f0292a32c557d4b8c2f2e62c4d13b1040ec0f6b2
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2024-04-08 00:18:47 +0200

    Implement info asset(s) (#5)

--- src/commands/info/mod.rs
+++ src/commands/info/mod.rs
@@ -2,6 +2,7 @@ use serde_json::Value;
 
 use crate::{context::Context, types::InfoSubcommand};
 
+mod assets;
 mod chain;
 mod path;
 
@@ -11,6 +12,11 @@ pub(crate) async fn execute_subcmd(
 ) -> Result<Value, anyhow::Error> {
     match subcmd {
         InfoSubcommand::Chain { chain_id } => chain::execute(ctx, chain_id).await,
+        InfoSubcommand::Assets { chain_id } => assets::execute(ctx, chain_id, None).await,
+        InfoSubcommand::Asset {
+            chain_id,
+            asset_name,
+        } => assets::execute(ctx, chain_id, Some(asset_name)).await,
         InfoSubcommand::Path { chain_a, chain_b } => path::execute(ctx, chain_a, chain_b).await,
     }
 }
