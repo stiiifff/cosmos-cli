1cb08514b5b89b675cb2098fd571ffbe5d26d43f
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2024-04-07 23:46:03 +0200

    Implement info chain (#4)

--- src/commands/info/mod.rs
+++ src/commands/info/mod.rs
@@ -2,6 +2,7 @@ use serde_json::Value;
 
 use crate::{context::Context, types::InfoSubcommand};
 
+mod chain;
 mod path;
 
 pub(crate) async fn execute_subcmd(
@@ -9,10 +10,7 @@ pub(crate) async fn execute_subcmd(
     subcmd: &InfoSubcommand,
 ) -> Result<Value, anyhow::Error> {
     match subcmd {
-        InfoSubcommand::Chain { chain_id } => {
-            println!("Getting information about chain {} ...", chain_id);
-            Ok(Value::Null)
-        }
+        InfoSubcommand::Chain { chain_id } => chain::execute(ctx, chain_id).await,
         InfoSubcommand::Path { chain_a, chain_b } => path::execute(ctx, chain_a, chain_b).await,
     }
 }
